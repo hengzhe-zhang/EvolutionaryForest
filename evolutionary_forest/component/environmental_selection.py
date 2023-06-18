3583ef3dda19b890ac65ddbb95ef0ce4093479da
Hengzhe Zhang <2970057811@qq.com>
2023-06-18 12:10:04 +1200

    GP Experiment

--- evolutionary_forest/component/environmental_selection.py
+++ evolutionary_forest/component/environmental_selection.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 
 from deap.tools import selNSGA2, sortNondominated, selSPEA2
 from numpy.linalg import norm
+from pymoo.mcdm.high_tradeoff import HighTradeoffPoints
 from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold
 
@@ -14,42 +15,11 @@ if TYPE_CHECKING:
 import numpy as np
 
 
-def distance_to_hyperplane(points, X):
-    points = np.array(points)
-    X = np.array(X)
-    A = points[0]
-    vectors = points[1:] - A
-
-    # Compute the SVD
-    U, S, V = np.linalg.svd(vectors)
-
-    # Obtain the normal vector from the last column of V
-    N = V[-1]
-
-    # Calculate the projection of X onto the hyperplane
-    t = np.dot(A - X, N) / np.dot(N, N)
-    P = X + t * N
-
-    # Compute the distance from X to the projection point P
-    distance = np.linalg.norm(X - P)
-    return distance
-
-
 def knee_point_detection(front):
-    # For maximization problem
-    front = np.array(front)
-    p_array = []
-    for dim in range(front.shape[1]):
-        id = np.argmax(front[:, dim])
-        p_array.append(front[id])
-    p_array = np.array(p_array)
-    ans = np.argmax([distance_to_hyperplane(p_array, x) for x in front])
-    # p1 = np.array([max(front[:, 0]), min(front[:, 1])])
-    # p2 = np.array([min(front[:, 0]), max(front[:, 1])])
-    # # 自动选择拐点
-    # ans = max([i for i in range(len(front))],
-    #           key=lambda i: norm(np.cross(p2 - p1, p1 - front[i])) / norm(p2 - p1))
-    return ans
+    ht = HighTradeoffPoints()
+    # convert to minimization
+    ans = ht.do(-1 * np.array(front))
+    return max(ans, key=lambda x: front[x][0])
 
 
 class EnvironmentalSelection():
@@ -154,15 +124,4 @@ class SPEA2(NSGA2):
 
 
 if __name__ == "__main__":
-    points = [
-        [1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1]
-    ]
-
-    X = [1, 1, 1, 1, 1]
-
-    distance = distance_to_hyperplane(points, X)
-    print("The distance between point X and the hyperplane is:", distance)
+    pass
