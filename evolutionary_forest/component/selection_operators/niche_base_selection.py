291ddf5b05bc91129035c128e4c29aff31fb27a8
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2023-10-04 15:16:47 +1300

    Clean Code

---
+++ evolutionary_forest/component/selection_operators/niche_base_selection.py
@@ -0,0 +1,22 @@
+import random
+
+from evolutionary_forest.component.selection import selAutomaticEpsilonLexicaseFast
+
+
+def niche_base_selection(individuals, k, key_objective=1):
+    # Sort individuals based on sharpness (worst to best)
+    sorted_individuals = sorted(individuals, key=lambda ind: ind.fitness.wvalues[key_objective])
+
+    # Segment the sorted individuals into niches (for simplicity, use sqrt(len(individuals)) niches)
+    num_niches = int(len(individuals) ** 0.5)
+    niche_size = len(individuals) // num_niches
+    niches = [sorted_individuals[i:i + niche_size] for i in range(0, len(sorted_individuals), niche_size)]
+
+    # Select k individuals using the niching selection
+    selected_individuals = []
+    while len(selected_individuals) < k:
+        niche_index = random.randint(0, num_niches - 1)
+        individuals = selAutomaticEpsilonLexicaseFast(niches[niche_index], 2)
+        selected_individuals.extend(individuals)
+
+    return selected_individuals[:k]
---
+++ evolutionary_forest/component/selection_operators/test/niche_base_selection.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+
+import evolutionary_forest.component.selection as evo_selection
+
+
+class MockIndividual:
+    def __init__(self, fitness_value):
+        self.fitness = MockFitness(fitness_value)
+
+
+class MockFitness:
+    def __init__(self, fitness_value):
+        self.wvalues = (0, fitness_value)
+
+
+class TestNicheBaseSelection(unittest.TestCase):
+    @patch.object(evo_selection, 'selAutomaticEpsilonLexicaseFast')
+    def test_niche_base_selection(self, mock_sel):
+        from evolutionary_forest.component.selection_operators.niche_base_selection import niche_base_selection
+
+        # Mock the return value of selAutomaticEpsilonLexicaseFast
+        mock_sel.side_effect = lambda x, k: x[:k]
+
+        # Create mock individuals with given fitness values
+        mock_individuals = [MockIndividual(i) for i in range(10)]
+        selected_individuals = niche_base_selection(mock_individuals, 3)
+
+        # Selecting 3 individuals,
+        self.assertEqual(len(selected_individuals), 3)
+        print(selected_individuals)
+
+
+if __name__ == '__main__':
+    unittest.main()
