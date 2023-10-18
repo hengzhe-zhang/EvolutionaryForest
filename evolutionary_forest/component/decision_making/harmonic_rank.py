53371fe13377346da206a234de31d8ff1d74f0d0
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2023-10-19 12:18:25 +1300

    Clean Code

--- evolutionary_forest/component/decision_making/harmonic_rank.py
+++ evolutionary_forest/component/decision_making/harmonic_rank.py
@@ -2,15 +2,17 @@ import numpy as np
 
 
 def best_harmonic_rank(front):
-    reciprocals = 1.0 / front
-    harmonic_means = front.shape[1] / np.sum(reciprocals, axis=1)
-    return np.argmax(harmonic_means)
+    # Rank within each column
+    ranks = front.argsort(axis=0).argsort(axis=0) + 1
+    reciprocals_of_ranks = 1.0 / ranks
+    harmonic_means_per_column = front.shape[1] / np.sum(reciprocals_of_ranks, axis=1)
+    return np.argmax(harmonic_means_per_column)
 
 
 if __name__ == '__main__':
     # Example usage:
-    front = np.array([[2, 4, 6],
-                      [1, 3, 9],
-                      [4, 5, 3]])
+    front = np.array([[2, 4],
+                      [1, 3],
+                      [4, 5]])
 
     print(best_harmonic_rank(front))
