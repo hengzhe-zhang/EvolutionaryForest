0cca1ddaa2ae75fa4adecc72d31553dbe24bf656
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-02-15 14:41:04 +1300

    Fix Bug

--- evolutionary_forest/component/selection.py
+++ evolutionary_forest/component/selection.py
@@ -1140,13 +1140,15 @@ def selLexicaseDCD(individuals, k):
 
 def selLexicaseKNN(individuals, k, neighbor=5):
     chosen = []
-    for i in range(0, k * 2, 2):
+    for i in range(0, k, 2):
         a: MultipleGeneGP = selAutomaticEpsilonLexicaseFast(individuals, 1)[0]
 
         # Calculate distances in semantic space
         distances = []
         for ind in individuals:
             dist = np.linalg.norm(a.case_values - ind.case_values)
+            if dist == 0:
+                continue
             distances.append((dist, ind))
 
         # Sort individuals based on distance
@@ -1156,6 +1158,7 @@ def selLexicaseKNN(individuals, k, neighbor=5):
         neighbors = [ind for _, ind in distances[:neighbor]]
 
         # best sharpness
+        chosen.append(a)
         chosen.append(max(neighbors, key=lambda x: x.fitness.wvalues[1]))
     return chosen
 
