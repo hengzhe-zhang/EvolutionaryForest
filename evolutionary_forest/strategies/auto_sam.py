abbdee03816f3d1a90c1aa4b7d8cb6308310cd05
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-02-17 17:11:14 +1300

    Fix Bug

--- evolutionary_forest/strategies/auto_sam.py
+++ evolutionary_forest/strategies/auto_sam.py
@@ -4,6 +4,26 @@ from sklearn.model_selection import KFold
 from sklearn.tree import DecisionTreeRegressor
 
 
+def calculate_adjusted_upper_bound(lb, ub, generalization_gap, mode="Increase"):
+    gap_mean = np.mean(generalization_gap)
+    gap_mean_clipped = np.clip(gap_mean, 0, 1)  # Clip the mean gap between 0 and 1
+
+    # Convert bounds to linear scale
+    lb_linear = np.log(lb)
+    ub_linear = np.log(ub)
+
+    # Calculate adjusted upper bound
+    if mode == "Increase":
+        adjusted_ub_linear = lb_linear + (ub_linear - lb_linear) * gap_mean_clipped
+    else:
+        adjusted_ub_linear = ub_linear - (ub_linear - lb_linear) * gap_mean_clipped
+
+    # Convert adjusted upper bound back to log scale
+    adjusted_ub = np.exp(adjusted_ub_linear)
+
+    return adjusted_ub
+
+
 def auto_tune_sam(X, y, std):
     _, lb, ub, criterion = std.split("-")
     ub = float(ub)
@@ -25,9 +45,17 @@ def auto_tune_sam(X, y, std):
         test_scores.append(test_score)
     if criterion == "GAP":
         # more gap, more penalty
-        return lb + (ub - lb) * max(np.mean(generalization_gap), 0)
+        return lb + (ub - lb) * np.clip(np.mean(generalization_gap), 0, 1)
     elif criterion == "Test":
         # high test score, less penalty
-        return ub - (ub - lb) * max(np.mean(test_scores), 0)
+        return ub - (ub - lb) * np.clip(np.mean(test_scores), 0, 1)
+    elif criterion == "LogGAP":
+        return calculate_adjusted_upper_bound(
+            lb, ub, np.mean(generalization_gap), mode="Increase"
+        )
+    elif criterion == "LogTest":
+        return calculate_adjusted_upper_bound(
+            lb, ub, np.mean(test_scores), mode="Decrease"
+        )
     else:
         raise Exception
