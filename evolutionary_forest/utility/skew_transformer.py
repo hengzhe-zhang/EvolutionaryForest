25f801fab14b6e47f4ac8a57441e1cd0120fb0f0
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-02-26 19:15:42 +1300

    Fix Bug

--- evolutionary_forest/utility/skew_transformer.py
+++ evolutionary_forest/utility/skew_transformer.py
@@ -8,73 +8,47 @@ class SkewnessCorrector(BaseEstimator, TransformerMixin):
         pass
 
     def fit(self, X, y=None):
-        # Reshape X to 2D if it's 1D
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-
-        # Compute skewness along each feature
+        X = X.reshape(-1, 1) if X.ndim == 1 else X
         self.skewness_ = skew(X, axis=0)
-        # Compute minimum value for each feature to apply shift if necessary
-        self.min_ = X.min(axis=0)
         return self
 
     def transform(self, X, y=None):
-        # Reshape X to 2D if it's 1D
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-        original_shape = X.shape
+        X = X.reshape(-1, 1) if X.ndim == 1 else X
+        X_trans = np.empty_like(X)
 
         for i in range(X.shape[1]):
-            shift = 0
-            if self.min_[i] < 0:
-                shift = -self.min_[i]  # Shift to make the minimum value 0
+            signs = np.sign(X[:, i])
+            abs_values = np.abs(X[:, i])
 
             if self.skewness_[i] > 0.5:
-                # Right-skewed, apply square root after shifting
-                X[:, i] = np.sqrt(X[:, i] + shift)
-                # Reverse the shift effect by subtracting the square root of the shift
-                X[:, i] = X[:, i] - np.sqrt(shift)
+                transformed = np.sqrt(abs_values)
             elif self.skewness_[i] < -0.5:
-                # Left-skewed, apply square after shifting
-                X[:, i] = (X[:, i] + shift) ** 2
-                # Reverse the shift effect by subtracting the squared shift (considering the data was squared)
-                X[:, i] = X[:, i] - shift**2
+                transformed = abs_values**2
+            else:
+                transformed = abs_values
 
-        # If the original input was 1D, return a 1D array
-        if original_shape[0] == 1 or original_shape[1] == 1:
-            return X.flatten()
-        else:
-            return X
+            X_trans[:, i] = transformed * signs
+
+        return X_trans.flatten() if X_trans.shape[1] == 1 else X_trans
 
     def inverse_transform(self, X, y=None):
-        # Reshape X to 2D if it's 1D
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-        original_shape = X.shape
+        X = X.reshape(-1, 1) if X.ndim == 1 else X
+        X_inv_trans = np.empty_like(X)
 
         for i in range(X.shape[1]):
-            shift = 0
-            if self.min_[i] < 0:
-                shift = -self.min_[i]  # Original shift to make the minimum value 0
+            signs = np.sign(X[:, i])
+            abs_values = np.abs(X[:, i])
 
             if self.skewness_[i] > 0.5:
-                # Data was right-skewed and underwent a square root transformation
-                # Add the square root of the shift, then square to reverse the transformation
-                X[:, i] = (X[:, i] + np.sqrt(shift)) ** 2
-                # Reverse the original shift
-                X[:, i] = X[:, i] - shift
+                inv_transformed = abs_values**2
             elif self.skewness_[i] < -0.5:
-                # Data was left-skewed and underwent a square transformation
-                # Add the squared shift, then take the square root to reverse the transformation
-                X[:, i] = np.sqrt(X[:, i] + shift**2)
-                # Reverse the original shift
-                X[:, i] = X[:, i] - shift
+                inv_transformed = np.sqrt(abs_values)
+            else:
+                inv_transformed = abs_values
 
-        # If the original input was 1D, return a 1D array
-        if original_shape[0] == 1 or original_shape[1] == 1:
-            return X.flatten()
-        else:
-            return X
+            X_inv_trans[:, i] = inv_transformed * signs
+
+        return X_inv_trans.flatten() if X_inv_trans.shape[1] == 1 else X_inv_trans
 
 
 if __name__ == "__main__":
