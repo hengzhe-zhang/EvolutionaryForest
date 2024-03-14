1e424948fc8c12fb5d5af29efde0c32e90b8daf1
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-03-14 16:22:43 +1300

    Fix Bug

--- evolutionary_forest/utility/tree_pool.py
+++ evolutionary_forest/utility/tree_pool.py
@@ -12,6 +12,7 @@ class TreePool:
         self.kd_tree = None  # This will be a cKDTree instance
         self.trees = []  # List to store PrimitiveTree objects
         self.tree_map = {}  # Maps tree indices to PrimitiveTree objects
+        self.normalized_semantics_list = []  # List to store normalized semantics
         self.seen_semantics = (
             set()
         )  # Set to store hashes of seen semantics for uniqueness
@@ -26,24 +27,22 @@ class TreePool:
             for semantics, tree in zip(ind.semantics.T, ind.gene):
                 semantics = semantics[
                     : self.semantics_length
-                ]  # Assuming you want to use the first 10 elements
+                ]  # Assuming you want to use the first semantics_length elements
                 # Normalize semantics and skip if norm is 0
                 norm = np.linalg.norm(semantics)
                 if norm == 0:
                     continue  # Skip this semantics as its norm is 0
 
                 normalized_semantics = normalize_vector(semantics)
-
-                # Create a hashable version of semantics for checking uniqueness
-                semantics_hash = hash(normalized_semantics.tostring())
-
-                # Skip if we've already seen these semantics
+                semantics_hash = tuple(normalized_semantics)
                 if semantics_hash in self.seen_semantics:
                     continue
 
-                # Add to our collections since it's unique and norm is not 0
                 self.seen_semantics.add(semantics_hash)
                 self.trees.append(tree)
+                self.normalized_semantics_list.append(
+                    normalized_semantics
+                )  # Store the normalized semantics
                 self.tree_map[len(self.trees) - 1] = tree
                 points.append(normalized_semantics)
 
@@ -52,20 +51,16 @@ class TreePool:
             excess = len(self.trees) - self.max_trees
             # Remove the oldest trees
             self.trees = self.trees[excess:]
-            # Update tree_map and seen_semantics accordingly
+            self.normalized_semantics_list = self.normalized_semantics_list[excess:]
             self.tree_map = {i: tree for i, tree in enumerate(self.trees)}
+            # Recreate seen_semantics based on the current normalized_semantics_list
             self.seen_semantics = {
-                hash(self.trees[i].semantics.tostring()) for i in range(len(self.trees))
+                tuple(semantics) for semantics in self.normalized_semantics_list
             }
 
-        # Create the KDTree with all collected points if there are any
+        # Create the KDTree with all collected points
         if points:
-            # If there are existing points in the kd-tree, include them as well
-            if self.kd_tree is not None:
-                all_points = np.vstack([self.kd_tree.data, points])
-            else:
-                all_points = np.array(points)
-            self.kd_tree = cKDTree(all_points)
+            self.kd_tree = cKDTree(self.normalized_semantics_list)
 
     def retrieve_nearest_tree(self, semantics: np.ndarray):
         if self.kd_tree is None:
