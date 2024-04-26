3f7e9bf691b42f196614cf1973b876a372e6cebd
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-04-26 23:57:23 +1200

    Add more features

--- evolutionary_forest/component/archive_operators/map_elites_archive.py
+++ evolutionary_forest/component/archive_operators/map_elites_archive.py
@@ -1,16 +1,19 @@
-from deap.tools import HallOfFame
+from deap.tools import HallOfFame, selBest
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import normalize
 
 
 class ACMAPElitesHOF(HallOfFame):
-    def __init__(self, maxsize):
+    def __init__(self, maxsize, map_archive_candidate_size=3):
         super().__init__(maxsize)
+        self.map_archive_candidate_size = map_archive_candidate_size
 
     def update(self, population):
-        semantics = [ind.predicted_values for ind in population] + [
-            ind.predicted_values for ind in self.items
-        ]
+        best_candidate = selBest(
+            population + list(self.items),
+            self.maxsize * self.map_archive_candidate_size,
+        )
+        semantics = [ind.predicted_values for ind in best_candidate]
 
         semantics_normalized = normalize(semantics, norm="l2")
 
@@ -20,13 +23,13 @@ class ACMAPElitesHOF(HallOfFame):
         labels = clustering.fit_predict(semantics_normalized)
 
         cluster_individuals = {i: [] for i in range(self.maxsize)}
-        for label, ind in zip(labels, population + list(self.items)):
+        for label, ind in zip(labels, best_candidate):
             cluster_individuals[label].append(ind)
 
         new_hof = []
         for cluster_inds in cluster_individuals.values():
             if cluster_inds:
-                best_individual = min(cluster_inds, key=lambda ind: ind.fitness.values)
+                best_individual = max(cluster_inds, key=lambda ind: ind.fitness.wvalues)
                 new_hof.append(best_individual)
 
         self.clear()
