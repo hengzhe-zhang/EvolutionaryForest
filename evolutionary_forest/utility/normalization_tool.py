9c8a8896370505520a8c9f9f66fd48f0f5e25b78
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-06-05 16:09:52 +1200

    Add more features

--- evolutionary_forest/utility/normalization_tool.py
+++ evolutionary_forest/utility/normalization_tool.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+from evolutionary_forest.model.cosine_kmeans import cosine_similarity
 
 
 def normalize_vector(v):
@@ -7,25 +12,37 @@ def normalize_vector(v):
         return v  # Return the zero vector as is
     normalized_v = v / norm
     # Find the first non-zero element
-    for i in range(len(normalized_v)):
-        if normalized_v[i] != 0:
-            # If the first non-zero element is negative, invert the vector
-            if normalized_v[i] < 0:
-                normalized_v = -normalized_v
-            break
-    return normalized_v
+    # for i in range(len(normalized_v)):
+    #     if normalized_v[i] != 0:
+    #         # If the first non-zero element is negative, invert the vector
+    #         if normalized_v[i] < 0:
+    #             normalized_v = -normalized_v
+    #         break
+    return normalized_v - normalized_v.mean()
 
 
 if __name__ == "__main__":
     # Example usage
-    residual_1 = np.array([-1, -1])
-    residual_2 = np.array([1, 1])
-    residual_3 = np.array([-1, 1])
+    residual_1 = np.array([0.9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    residual_2 = np.array([-0.1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
 
     normalized_residual_1 = normalize_vector(residual_1)
     normalized_residual_2 = normalize_vector(residual_2)
-    normalized_residual_3 = normalize_vector(residual_3)
 
-    print(normalized_residual_1)  # Output should be [1/sqrt(2), 1/sqrt(2)]
-    print(normalized_residual_2)  # Output should be [1/sqrt(2), 1/sqrt(2)]
-    print(normalized_residual_3)  # Output should be [-1/sqrt(2), 1/sqrt(2)]
+    print(normalized_residual_1)
+    print(normalized_residual_2)
+
+    print(cosine_similarity(normalized_residual_1, normalized_residual_2))
+    print(pearsonr(normalized_residual_1, normalized_residual_2)[0])
+
+    regression = LinearRegression()
+    regression.fit(normalized_residual_1.reshape(-1, 1), normalized_residual_2)
+    print(regression.predict(normalized_residual_1.reshape(-1, 1)))
+    print(
+        "R2",
+        r2_score(
+            normalized_residual_2,
+            regression.predict(normalized_residual_1.reshape(-1, 1)),
+        ),
+    )
+    print(r2_score(normalized_residual_2, -1 * normalized_residual_1))
