ee5c67f6d4f9f7cb9843cebeae01c99778fc45af
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2024-09-28 12:34:15 +1200

    Clean Code

--- evolutionary_forest/utility/mlp_library.py
+++ evolutionary_forest/utility/mlp_library.py
@@ -1410,7 +1410,7 @@ class NeuralSemanticLibrary(nn.Module):
     def get_mask(self, nearest_x):
         nearest_x_norm = F.normalize(nearest_x, dim=1)
         threshold = 0.95
-        mask = torch.matmul(nearest_x_norm, nearest_x_norm.T) < threshold
+        mask = torch.abs(torch.matmul(nearest_x_norm, nearest_x_norm.T)) < threshold
         mask = mask.fill_diagonal_(True)
         # (torch.sum(mask == False) > 0).item()
         return mask
