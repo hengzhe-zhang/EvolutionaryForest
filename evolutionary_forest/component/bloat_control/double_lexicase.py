049be8045bcc2253c7d3e3869d046b1a77bb1292
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2025-10-19 23:58:13 +1300

    Format All codes

--- evolutionary_forest/component/bloat_control/double_lexicase.py
+++ evolutionary_forest/component/bloat_control/double_lexicase.py
@@ -4,32 +4,38 @@ from scipy.special import softmax
 from evolutionary_forest.component.selection import selAutomaticEpsilonLexicaseFast
 
 
+def get_individual_size(x):
+    """Return a size metric suitable for minimization."""
+    if (
+        hasattr(x, "fitness")
+        and hasattr(x.fitness, "wvalues")
+        and len(x.fitness.wvalues) > 1
+    ):
+        # Negate because DEAP maximizes by default
+        return -x.fitness.wvalues[1]
+    return sum(len(tree) for tree in x.gene)
+
+
 def doubleLexicase(pop, k, lexicase_round=10, size_selection="Roulette"):
     chosen = []
-    objective_std = np.std([x.fitness.wvalues[1] for x in pop])
+    objective_std = np.std([get_individual_size(x) for x in pop])
+
     for _ in range(k):
         candidates = selAutomaticEpsilonLexicaseFast(pop, lexicase_round)
-        if hasattr(candidates[0], "fitness") and len(candidates[0].fitness.wvalues) > 1:
-            # For bi-object optimization
-            size_arr = [x.fitness.wvalues[1] for x in candidates]
-            # change maximize to minimize
-            size_arr = np.array([-x for x in size_arr])
-        else:
-            size_arr = np.array([len(x) for x in candidates])
+        size_arr = np.array([get_individual_size(x) for x in candidates])  # minimize
+
         if size_selection == "Softmax":
             # because it's a minimization problem, we need to convert back
             if objective_std > 0:
                 size_arr /= objective_std
-            index = np.random.choice(
-                [i for i in range(0, len(size_arr))], p=softmax(-1 * size_arr)
-            )
+            index = np.random.choice(range(len(size_arr)), p=softmax(-1 * size_arr))
         elif size_selection == "Roulette":
             size_arr = np.max(size_arr) + np.min(size_arr) - size_arr
             if size_arr.sum() <= 0:
-                index = np.random.choice([i for i in range(0, len(size_arr))])
+                index = np.random.choice(range(len(size_arr)))
             else:
                 index = np.random.choice(
-                    [i for i in range(0, len(size_arr))], p=size_arr / size_arr.sum()
+                    range(len(size_arr)), p=size_arr / size_arr.sum()
                 )
         elif size_selection == "Min":
             index = np.argmin(size_arr)
