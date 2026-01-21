a94549cef09e061c5bdd84fcaa4d28ca803e9f9a
Hengzhe Zhang <hengzhe.zhang@ecs.vuw.ac.nz>
2026-01-22 10:39:43 +1300

    Format All codes

--- evolutionary_forest/forest.py
+++ evolutionary_forest/forest.py
@@ -6541,12 +6541,20 @@ class EvolutionaryForestRegressor(RegressorMixin, TransformerMixin, BaseEstimato
                 count += learner.tree_.node_count
             elif isinstance(learner, (OptimalKNNRandomDT)):
                 count += learner.initial_model_.tree_.node_count
-                for weight in learner.knn_model_.weights:
-                    count += weight.shape[0]
+                # OptimalKNN and its subclasses (PLSKNN, LPPKNN) use 'weight' (singular)
+                if (
+                    hasattr(learner.knn_model_, "weight")
+                    and learner.knn_model_.weight is not None
+                ):
+                    count += learner.knn_model_.weight.shape[0]
             elif isinstance(learner, (RidgeBoostedKNN)):
                 count += learner.ridge_model_.coef_.shape[0]
-                for weight in learner.knn_model_.weights:
-                    count += weight.shape[0]
+                # OptimalKNN and its subclasses (PLSKNN, LPPKNN) use 'weight' (singular)
+                if (
+                    hasattr(learner.knn_model_, "weight")
+                    and learner.knn_model_.weight is not None
+                ):
+                    count += learner.knn_model_.weight.shape[0]
                 if isinstance(learner.knn_model_.knn, DecisionTreeRegressor):
                     count += learner.knn_model_.knn.tree_.node_count
             elif isinstance(learner, (RidgeDTPlus)):
